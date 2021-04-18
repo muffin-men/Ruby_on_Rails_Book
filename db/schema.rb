c7cc26351de94fee7c8451e3958a626dd94e8c96
muffin <whitenaito45@gmail.com>
2021-04-18 17:05:20 +0900

    add_NOTNULL

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_111640) do
+ActiveRecord::Schema.define(version: 2021_04_18_071940) do
 
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
